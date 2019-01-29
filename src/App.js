36fea25fd072ba1cf0bb948a4b186fa5e5ce92c0
gimjudge <gimjudge@hotmail.com>
2019-01-29 12:17:47 -0800

    Search Feature

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ export default class App extends Component {
   } 
 
   componentDidMount() {
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+  }
+
+  performSearch = (query) => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
           gifs: response.data.data
@@ -25,13 +28,14 @@ export default class App extends Component {
       });
   }
 
+
   render() { 
     return (
       <div>
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch} />      
           </div>   
         </div>    
         <div className="main-content">
